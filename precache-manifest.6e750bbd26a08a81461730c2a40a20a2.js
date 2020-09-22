d837f97aa9bf22f879d03e630742495dc7f4d4c3
zizimoos_iMac <zizimoos@gmail.com>
2020-09-22 13:47:44 +0900

    Updates

---
+++ precache-manifest.6e750bbd26a08a81461730c2a40a20a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b919f63661aa30006dc824270484e1ea",
+    "url": "/bcwitter/index.html"
+  },
+  {
+    "revision": "7401c5b35a2db50fecf4",
+    "url": "/bcwitter/static/css/main.51c00765.chunk.css"
+  },
+  {
+    "revision": "91b20fd67266627bbd13",
+    "url": "/bcwitter/static/js/2.afba0ada.chunk.js"
+  },
+  {
+    "revision": "6524af321f8d3e987e59afab1834492e",
+    "url": "/bcwitter/static/js/2.afba0ada.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7401c5b35a2db50fecf4",
+    "url": "/bcwitter/static/js/main.1ebb76a1.chunk.js"
+  },
+  {
+    "revision": "f8f874d3a50bf402cf1e",
+    "url": "/bcwitter/static/js/runtime-main.3d97e2e2.js"
+  }
+]);
